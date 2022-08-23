5bce285a297bf5fe18b2fc82511d03df289cf79c
Juan Villegas <--global>
2022-08-22 20:30:11 -0500

    I have add the seed planting

--- index.js
+++ index.js
@@ -1,68 +1,77 @@
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
 const w = 450*2
 const h = 270*2
 const padding = 10
 
-const seed = {
-    width: w/32,
-    height: h/16,
-    XYPosition: [(w-30), (h/40)],
-    color: '#EE9D5D',
+
+class Drawing {
+    constructor(width,height,XYPosition,color,roundness){
+        this.width=width
+        this.height=height
+        this.XYPosition=XYPosition
+        this.color=color
+        this.roundness=roundness
+    }
+    draw(){
+        context.beginPath()
+        context.roundRect(this.XYPosition[0],this.XYPosition[1],this.width,this.height,[this.roundness])
+        context.fillStyle = this.color
+        context.fill()
+    }
 }
 
-const myGameArea = {
-    canvasGame: document.createElement('canvas'),
-    hi: function(){console.log("hi")},
-    start: function() {
-        this.canvasGame.width = w
-        this.canvasGame.height = h
-
-        this.contextGame = this.canvasGame.getContext('2d')
-        
-        //Drawing canva
-        this.contextGame.fillStyle = '#C0D889'
-        this.contextGame.fillRect(w/4,h/4,w/2,h/2)
-
-        //Drawing garden
-
-        this.contextGame.roundRect(w/4,h/4,w/2,h/2,2.5)
-        this.contextGame.lineWidth = padding
-        this.contextGame.strokeStyle = 'white'
-        this.contextGame.stroke()
-
-        //Drawing grass
-        const grassWidth = w/8
-        const grassHeight = h/8
-        const grassXPosition = (w/4+5) - padding
-        const grassYPosition = (h/4+5) - padding
-        let xOffset = grassXPosition
-        let yOffset = grassYPosition
-        const positions = [{x:0,y:0},{x:0,y:2},{x:2,y:0},{x:2,y:2},{x:1,y:1},{x:1,y:3},{x:3,y:1},{x:3,y:3}]
-        for(let i=0; i<positions.length; i++){
-            xOffset=grassXPosition+(grassWidth*positions[i].x*.97)+padding
-            yOffset=grassYPosition+(grassHeight*positions[i].y*.95)+padding
-            this.contextGame.beginPath()
-            this.contextGame.roundRect(xOffset,yOffset,grassWidth,grassHeight,[10])
-            this.contextGame.fillStyle ='#B1C07D'
-            this.contextGame.fill()
-        }
-
-        //Drawing seeds
-        const bean = Object.create(seed)
-        this.contextGame.beginPath()
-        this.contextGame.roundRect(bean.XYPosition[0],bean.XYPosition[1],bean.width,bean.height,[10])
-        this.contextGame.fillStyle = bean.color
-        this.contextGame.fill()
-
-        hi()
-
-        document.body.insertBefore(this.canvasGame,document.body.childNodes[0])
-    },
+class Seed extends Drawing{
+    constructor(width,height,XYPosition,color,roundness){
+        super(width,height,XYPosition,color,roundness)
+    }
 }
 
-
-
+class Tile extends Drawing{
+    constructor(width,height,XYPosition,color,roundness){
+        super(width,height,XYPosition,color,roundness)
+    }
+}
 
 const startGame = () => {
-    myGameArea.start();
-    window.resizeTo(screen.width-300,screen.height-500)   
+    canvas.width = w
+    canvas.height = h
+
+    //Drawing canva
+    context.fillStyle = '#C0D889'
+    context.fillRect(w/4,h/4,w/2,h/2)
+
+    //Drawing garden
+    context.roundRect(w/4,h/4,w/2,h/2,2.5)
+    context.lineWidth = padding
+    context.strokeStyle = 'white'
+    context.stroke()
+
+    //Drawing grass HACERLA UN OBJETO
+
+    const grassWidth = w/8
+    const grassHeight = h/8
+    const grassXPosition = (w/4+5) - padding
+    const grassYPosition = (h/4+5) - padding
+    let xOffset = grassXPosition
+    let yOffset = grassYPosition
+    const positions = [{x:0,y:0},{x:0,y:2},{x:2,y:0},{x:2,y:2},{x:1,y:1},{x:1,y:3},{x:3,y:1},{x:3,y:3}]
+    for(let i=0; i<positions.length; i++){
+        xOffset=grassXPosition+(grassWidth*positions[i].x*.97)+padding
+        yOffset=grassYPosition+(grassHeight*positions[i].y*.95)+padding
+        context.beginPath()
+        context.roundRect(xOffset,yOffset,grassWidth,grassHeight,[10])
+        context.fillStyle ='#B1C07D'
+        context.fill()
+    }
+
+
+
+
+    //Drawing seeds
+    const bean = new Seed(w/30,h/16,[(w-40), (h/40)],'#EF9E63',100)
+    bean.draw()
 }
+
+
+
